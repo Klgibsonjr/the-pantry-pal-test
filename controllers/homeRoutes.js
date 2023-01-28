8a2583c940689953a604a8900d32c6aee74ba050
klgibsonjr <klgibsonjr@gmail.com>
2023-01-28 12:21:01 -0800

    saved file

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,3 +1,5 @@
 const router = require('express').Router();
-const { Category, Item, User, UserItem } = require('../../models');
-const withAuth = require('../../utils/auth');
+const { Category, Item, User, UserItem } = require('../models');
+const withAuth = require('../utils/auth');
+
+router.get('/', (req, res) => {});
